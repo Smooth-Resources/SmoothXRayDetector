59242a48470846947a5a093ac07e808c6cf413f1
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2025-06-05 20:03:40 +0200

    feat: add empty user service (WIP)

---
+++ src/main/java/com/smoothresources/smoothxraydetector/service/UserService.java
@@ -0,0 +1,13 @@
+package com.smoothresources.smoothxraydetector.service;
+
+import com.smoothresources.smoothxraydetector.entity.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class UserService {
+
+    public Optional<User> getUserByUUID(UUID uuid) {
+
+    }
+}
