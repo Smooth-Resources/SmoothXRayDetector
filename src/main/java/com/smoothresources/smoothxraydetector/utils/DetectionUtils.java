97514f79c6252dafb6fbf77ee242588f41de0c72
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2025-06-05 20:03:50 +0200

    feat: add detection utils

---
+++ src/main/java/com/smoothresources/smoothxraydetector/utils/DetectionUtils.java
@@ -0,0 +1,55 @@
+package com.smoothresources.smoothxraydetector.utils;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import com.smoothresources.smoothbase.paper.file.PaperYAMLFile;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.World;
+
+import java.util.Optional;
+
+public class DetectionUtils {
+
+    @Inject @Named("config")
+    private PaperYAMLFile config;
+
+    public Optional<String> getDetectionIdByMaterialAndWorld(Material material, World world) {
+        String[] detections = (String[]) config.getNode("detections").childrenMap().keySet().stream().map(Object::toString).toArray();
+        for (String detection : detections) {
+            Material detectionMaterial = config.getMaterial("detections", detection, "material");
+            if (!material.equals(detectionMaterial)) continue;
+
+            World detectionWorld = Bukkit.getWorld(config.getString("detections", detection, "world"));
+            if (detectionWorld == null || detectionWorld.getUID() != world.getUID()) continue;
+
+            return Optional.of(detection);
+        }
+
+        return Optional.empty();
+    }
+
+    public int getMinBlockBetweenLastByDetection(String detection) {
+        return config.getInt("detections", detection, "requirements", "min-blocks-between-last");
+    }
+
+    public int getMaxYByDetection(String detection) {
+        return config.getInt("detections", detection, "requirements", "max-y");
+    }
+
+    public int getMinYByDetection(String detection) {
+        return config.getInt("detections", detection, "requirements", "min-y");
+    }
+
+    public int getAverageMinSamplesByDetection(String detection) {
+        return config.getInt("detections", detection, "average-calculation", "min-samples");
+    }
+
+    public int getAverageMaxSamplesByDetection(String detection) {
+        return config.getInt("detections", detection, "average-calculation", "max-samples");
+    }
+
+    public double getAverageMinSecondsByDetection(String detection) {
+        return config.getDouble("detections", detection, "average-calculation", "min-seconds");
+    }
+}
